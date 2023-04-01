191185581e1e2f02135f6a07a70533c6c13c95ed
Ilie Petre-Cristian <petre.ilie@s.unibuc.ro>
2023-04-01 12:10:35 +0300

    Initial commit

--- app/src/main/java/com/cav/hackathon/MainActivity.kt
+++ app/src/main/java/com/cav/hackathon/MainActivity.kt
@@ -3,13 +3,18 @@ package com.cav.hackathon
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.Surface
-import androidx.compose.material3.Text
+import androidx.compose.foundation.layout.*
+import androidx.compose.material3.*
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.input.PasswordVisualTransformation
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import com.cav.hackathon.ui.theme.HackathonCSTTheme
 
 class MainActivity : ComponentActivity() {
@@ -22,7 +27,9 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 ) {
-                    Greeting("Android")
+                    Column {
+                        SignUpScreen()
+                    }
                 }
             }
         }
@@ -38,6 +45,67 @@ fun Greeting(name: String) {
 @Composable
 fun DefaultPreview() {
     HackathonCSTTheme {
-        Greeting("Android")
+        Column(verticalArrangement = Arrangement.SpaceEvenly) {
+            Greeting("Android")
+        }
+    }
+}
+
+@Composable
+fun SignUpScreen() {
+    val nameState = remember { mutableStateOf("") }
+    val emailState = remember { mutableStateOf("") }
+    val passwordState = remember { mutableStateOf("") }
+    val confirmPasswordState = remember { mutableStateOf("") }
+
+    Column(
+        modifier = Modifier
+            .fillMaxSize()
+            .padding(vertical = 16.dp, horizontal = 48.dp),
+        horizontalAlignment = Alignment.CenterHorizontally,
+        verticalArrangement = Arrangement.Center
+    ) {
+        Text(text = "Sign Up", fontSize = 48.sp, fontWeight = FontWeight.Bold)
+        Spacer(modifier = Modifier.height(100.dp))
+
+        TextField(
+            value = nameState.value,
+            onValueChange = { nameState.value = it },
+            label = { Text("Name") },
+            modifier = Modifier.fillMaxWidth()
+        )
+        Spacer(modifier = Modifier.height(16.dp))
+
+        TextField(
+            value = emailState.value,
+            onValueChange = { emailState.value = it },
+            label = { Text("Email") },
+            modifier = Modifier.fillMaxWidth()
+        )
+        Spacer(modifier = Modifier.height(16.dp))
+
+        TextField(
+            value = passwordState.value,
+            onValueChange = { passwordState.value = it },
+            label = { Text("Password") },
+            modifier = Modifier.fillMaxWidth(),
+            visualTransformation = PasswordVisualTransformation()
+        )
+        Spacer(modifier = Modifier.height(16.dp))
+
+        TextField(
+            value = confirmPasswordState.value,
+            onValueChange = { confirmPasswordState.value = it },
+            label = { Text("Confirm Password") },
+            modifier = Modifier.fillMaxWidth(),
+            visualTransformation = PasswordVisualTransformation()
+        )
+        Spacer(modifier = Modifier.height(128.dp))
+
+        Button(
+            onClick = { /* Handle sign up action */ },
+        ) {
+            Text(text = "Sign Up", modifier = Modifier.padding(16.dp))
+        }
     }
 }
